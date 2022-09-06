c07a92293b17b6774e4441229872dc0159f96bbf
elao135 <pak5553@naver.com>
2022-09-06 13:28:26 +0900

    2nd modf

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cona/final_ojt/src
-# Build directory: /home/cona/final_ojt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("OJT_SCBOT")
-subdirs("whCtrl_pkg")
--- build/OJT_SCBOT/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cona/final_ojt/src/OJT_SCBOT
-# Build directory: /home/cona/final_ojt/build/OJT_SCBOT
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
